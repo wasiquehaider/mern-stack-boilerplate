81cdfea96b030854f3bc9023526ce9acfb03c12d
Wasique Haider <wasiquehaider02@gmail.com>
2023-05-03 21:09:11 +0500

    abse url for backend

--- frontend/src/api/dynamicAPI.js
+++ frontend/src/api/dynamicAPI.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { config } from '../utils/customLocalStorage'
 
-const base_url = "http://ec2-52-15-137-187.us-east-2.compute.amazonaws.com:5000"
+const base_url = "http://ec2-3-22-74-218.us-east-2.compute.amazonaws.com:5000"
+// const base_url = "http://localhost:5000"
 
 const dynamicAPI = async (method, url, obj = {}) => {
   try {
